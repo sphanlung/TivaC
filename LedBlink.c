74581c57a4874bba32e0bb8e3a59478c1573e185
Sergio PL <sergio.potati@gmail.com>
2015-04-05 14:00:12 -0500

    Update LedBlink.c
    Fixing comments

--- LedBlink.c
+++ LedBlink.c
@@ -13,7 +13,9 @@
 //
 //***************************************************************************************
 
-#include "tm4c123gh6pm.h"
+#include "tm4c123gh6pm.h"	// Header file accesses registers in TMC123 microcontroller
+
+//define easy to read names for registers
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
 #define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
@@ -25,7 +27,6 @@
 #define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 
-// 2. Declarations Section
 //   Global Variables
 unsigned long SW1, SW2;  	// input from PF0 and PF4
 
@@ -35,28 +36,29 @@ void Delay(void);
 
 // 3. Subroutines Section
 int main(void){    
-  PortF_Init();        		// Call initialization of port PF0-PF4    
+  PortF_Init();        			// Call initialization of port PF0-PF4    
   while(1){
-    SW1 = GPIO_PORTF_DATA_R&0x10; // read PF4 into SW1
-		SW2 = GPIO_PORTF_DATA_R&0x01; // read PF0 into SW2
-    if(SW1 == 0x00){              // zero means SW1 is pressed
-      GPIO_PORTF_DATA_R = 0x08;   // LED is green
-			} 
-		else if(SW2 == 0x00){                      // 0x10 means SW1 is not pressed
-      GPIO_PORTF_DATA_R = 0x02;  // LED is red
-			}
-    Delay();                     // wait 0.1 sec
-    GPIO_PORTF_DATA_R = 0x04;    // LED is blue
-    Delay();                     // wait 0.1 sec
+    SW1 = GPIO_PORTF_DATA_R&0x10; 	// read PF4 into SW1
+    SW2 = GPIO_PORTF_DATA_R&0x01; 	// read PF0 into SW2
+    if(SW1 == 0x00){              	// zero means SW1 is pressed
+      GPIO_PORTF_DATA_R = 0x08;   	// LED is green
+     } 
+    else if(SW2 == 0x00){               // 0x10 means SW1 is not pressed
+      GPIO_PORTF_DATA_R = 0x02;  	// LED is red
+     }
+    Delay();				// wait 0.1 sec
+    GPIO_PORTF_DATA_R = 0x04;    	// LED is blue
+    Delay();                     	// wait 0.1 sec
   }
 }
 
 // Subroutine to initialize port F pins for input and output
 // PF4 and PF0 are input SW1 and SW2 respectively
 // PF3,PF2,PF1 are outputs to the LED
-void PortF_Init(void){ volatile unsigned long delay;
+void PortF_Init(void){ 
+  volatile unsigned long delay;
   SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
-  delay = SYSCTL_RCGC2_R;           // delay   
+  delay = SYSCTL_RCGC2_R;           // reading register adds a delay   
   GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
   GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
   GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
